90bf520b96fff4f128dc6d99687443700ce1af2c
Darren Colby <dscolby17@gmail.com>
2023-06-22 22:21:30 -0500

    Added files from other repo

---
+++ pages/settings.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import time
+
+BODY =  """
+          The settings below are parameters that will be passed to the OpenAI Whisper model 
+          to transcribe the text into English. If you are unsure of what they mean, you can 
+          leave them the way they are, as they should work well in most cases. You can also
+          experiment with different settings. For more information see the OpenAI Whisper
+          [GitHub page](https://github.com/openai/whisper).
+
+        """
+
+# Changes the width of the form so there is not a ton of whitespace
+CSS = """
+        <style>
+        section.main > div {max-width:75rem}
+        </style>
+     """
+
+# Changes the default navicon in the browser tab
+st.set_page_config(page_title="Transcription Settings")
+
+st.markdown(CSS, unsafe_allow_html=True)
+
+def save_settings():
+    """Callback function to set the session state with settings to be passed to the Whisper 
+        model
+    """
+    st.session_state.whisper_model = model
+    st.session_state.temperature = temperature
+    st.session_state.temperature_increment_on_fallback = temp_inc
+    st.session_state.no_speech_threshold = no_speech
+    st.session_state.logprob_threshold = logprob
+    st.session_state.compression_ratio_threshold = compression
+    st.session_state.condition_on_previous_text = previous_text
+    st.session_state.word_timestamps = timestamps
+    st.session_state.verbose = verbose
+
+
+st.title("Transcription Settings")
+
+st.write(BODY)
+
+with st.form("Model Settings"):
+    model = st.selectbox(label="Model", 
+                         options=('tiny', 'base', 'small', 'medium', 'large'), 
+                         key="whisper_model", index=1)
+    temperature = st.number_input("Temperature", 0.00, 1.00, 0.00, step=0.10, 
+                                  key="temperature")
+    temp_inc = st.number_input("Temperature Increment On Fallback", 0.00, 1.00, 0.20, 
+                               step=0.10, key="temperature_increment_on_fallback")
+    no_speech = st.number_input("No Speech Threshold", 0.00, 1.00, 0.60, 
+                               step=0.10, key="no_speech_threshold")
+    logprob = st.number_input("Log Probability Threshold", -20.0, 0.0, -1.0, 
+                               step=0.50, key="logprob_threshold")
+    compression = st.number_input("Compression Ratio Threshold", 0.0, 10.0, 2.40, 
+                               step=0.20, key="compression_ratio_threshold")
+    previous_text = st.checkbox("Condition On Previous Text", 
+                                key="condition_on_previous_text", value=True)
+    timestamps = st.checkbox("Timestamps", key="word_timestamps", value=True)
+    verbose = st.checkbox("Verbose", key="verbose", value=False)
+    
+    save_settings = st.form_submit_button("💾 Save Settings", on_click=save_settings)
+
+    # Show a success message and clear it after five seconds
+    success = st.success("Settings Saved!")
+    time.sleep(5)
+    success.empty()
---
+++ pages/transcription.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import os
+import whisper
+
+BODY =  """
+          The settings below are parameters that will be passed to the OpenAI Whisper model 
+          to transcribe the text into English. If you are unsure of what they mean, you can 
+          leave them the way they are, as they should work well in most cases. You can also
+          experiment with different settings. For more information see the OpenAI Whisper
+          [GitHub page](https://github.com/openai/whisper).
+
+        """
+
+# Changes the width of the form so there is not a ton of whitespace
+CSS = """
+        <style>
+        section.main > div {max-width:75rem}
+        </style>
+     """
+
+st.session_state.update(st.session_state)
+
+# Changes the default navicon in the browser tab
+st.set_page_config(page_title="Transcription Settings")
+
+st.markdown(CSS, unsafe_allow_html=True)
+
+uploaded_files = st.file_uploader("Choose one or more audio files")
+
+model = whisper.load_model(st.session_state.whisper_model)
+
+#with open("audio/" + uploaded_files.name,"wb") as f: 
+      #f.write(uploaded_files) 
+
+#result = model.transcribe("audio.mp3", **st.session_state)
+
+#st.text(result["text"])
+
+#st.text(st.session_state.whisper_model)
