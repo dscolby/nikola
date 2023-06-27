62af4a4f4cf2d84c9cd50efa722842d0b296e08e
Darren Colby <dscolby17@gmail.com>
2023-06-26 22:15:12 -0500

    Updated caching parameters

--- pages/2_📝_Transcribe_Audio.py
+++ pages/2_📝_Transcribe_Audio.py
@@ -56,7 +56,7 @@ def set_defaults():
     st.session_state.verbose = False
 
 
-@st.cache(show_spinner=False, ttl=5)
+@st.cache(show_spinner=False)
 def transcribe_recording(re):
     """
     Transcribe uploaded text with OpenAI Whisper
