9f2be5915b8645a7ca226e38daf61b89e88515fe
Darren Colby <dscolby17@gmail.com>
2023-06-27 14:47:30 -0500

    Changed logic again

--- pages/1_⚙️_Transcription_Settings.py
+++ pages/1_⚙️_Transcription_Settings.py
@@ -55,7 +55,6 @@ def save_settings():
     st.session_state.compression_ratio_threshold = compression
     st.session_state.condition_on_previous_text = previous_text
     st.session_state.word_timestamps = timestamps
-    next_page()
 
 
 def model_settings():
@@ -85,9 +84,9 @@ def model_settings():
         st.form_submit_button("💾 Save Settings", on_click=save_settings)
 
         # Show a success message and clear it after five seconds
-        #success = st.success("Settings Saved!")
-        #time.sleep(3)
-        #success.empty()
+        success = st.success("Settings Saved!")
+        time.sleep(3)
+        success.empty()
 
 
 def next_page():
@@ -101,4 +100,4 @@ def next_page():
 if __name__ == '__main__':
     setup()
     model_settings()
-    #next_page()
+    next_page()
