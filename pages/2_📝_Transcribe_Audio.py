0a5da425dc5ffa007472f9b407522389b0ea36fa
Darren Colby <dscolby17@gmail.com>
2023-06-27 13:49:06 -0500

    Changed spinner message

--- pages/2_📝_Transcribe_Audio.py
+++ pages/2_📝_Transcribe_Audio.py
@@ -91,7 +91,7 @@ def transcribe_with_spinner():
         with open(filepath,"wb") as f:
             f.write(audio_file.getbuffer())
 
-        with st.spinner("Transcription in progress"):
+        with st.spinner(f"Transcribing with {st.session_state.whisper_model} model"):
             transcribe_recording(filepath)
         del audio_file
 
