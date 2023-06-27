c8f735205247fdb5ee4547364ef18bbe0b6d62a7
Darren Colby <dscolby17@gmail.com>
2023-06-27 14:27:03 -0500

    Updated session state logic

--- pages/1_⚙️_Transcription_Settings.py
+++ pages/1_⚙️_Transcription_Settings.py
@@ -54,6 +54,7 @@ def save_settings():
     st.session_state.compression_ratio_threshold = compression
     st.session_state.condition_on_previous_text = previous_text
     st.session_state.word_timestamps = timestamps
+    st.session_state.disabled = True
 
 
 def model_settings():
@@ -80,7 +81,8 @@ def model_settings():
                                     key="condition_on_previous_text", value=True)
         timestamps = st.checkbox("Timestamps", key="word_timestamps", value=True)
     
-        st.form_submit_button("💾 Save Settings", on_click=save_settings, disabled=True)
+        st.form_submit_button("💾 Save Settings", on_click=save_settings, 
+                              disabled=st.session_state.disabled)
 
         # Show a success message and clear it after five seconds
         success = st.success("Settings Saved!")
@@ -97,6 +99,8 @@ def next_page():
 
 
 if __name__ == '__main__':
+    st.session_state.disabled = False
+
     setup()
     model_settings()
     next_page()
